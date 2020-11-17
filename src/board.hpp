7a3f5f3924ab28772e88b534ddd11449b8644c61
SmiteshP <smiteshpatil2010@gmail.com>
2020-11-17 22:18:42 +0530

    Add more functions to board class

--- src/board.hpp
+++ src/board.hpp
@@ -11,22 +11,50 @@ public:
 	std::array<std::array <int, 4>, 4> look_down();
 
 	// make respective move
-	void move_left();
-	void move_up();
-	void move_right();
-	void move_down();
+	// return true if succesful else false
+	bool move_left();
+	bool move_up();
+	bool move_right();
+	bool move_down();
 
 	// undo and redo
 	// return true if succesful else false
-	void undo();
-	void redo();
+	bool undo();
+	bool redo();
 
 private:
+	// stores the current state of board
+	std::array <std::array <int, 4>, 4> curr_board;
+
+	// stores immediate next board states
+	std::array <std::array <int, 4>, 4> next_left;
+	std::array <std::array <int, 4>, 4> next_up;
+	std::array <std::array <int, 4>, 4> next_right;
+	std::array <std::array <int, 4>, 4> next_down;
+
+	int curr_max;
+	int next_max;
+	int curr_sum;
+
 	// store game board history
 	state_queue <std::array <std::array <int, 4>, 4>> grid;
 	// store random numbers to compute/recompute board
 	state_queue <std::pair <int, int>> random_numbers;
 
-	// colapse the list of numbers from left to right
-	void colapse(std::array<int *, 4> arr);
+	// collapse the list of numbers from left to right
+	void collapse(std::array<int *, 4> arr);
+
+	// Randomly add tile
+	// return true if tile added succesfully
+	// return false if tile cannot be added
+	bool add_random_tile(std::array <std::array <int, 4>, 4>& grid);
+
+	// Calculate max of next states
+	void next_grid_max();
+
+	// Calculates the diffent state->Z operations on curr_state
+	// Max tile
+	// Sum
+	// Set of tiles and their count
+	void update_state(const std::array <std::array <int, 4>, 4>& grid);
 };
