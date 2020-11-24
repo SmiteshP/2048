18cd4b947a0ef1ea45d2688943dd5114e0e60ec9
SmiteshP <smiteshpatil2010@gmail.com>
2020-11-24 08:22:48 +0530

    Fix minor formatting erros

--- src/board.cpp
+++ src/board.cpp
@@ -15,16 +15,16 @@ board::board() {
             curr_board[i][j] = 0;
         }
     }
-    
+
     add_random_tile();
     add_random_tile();
-    
+
     score = 0;
     score_increment_left = 0;
     score_increment_right = 0;
     score_increment_up = 0;
     score_increment_down = 0;
-    
+
     grid_queue =
         new state_queue<std::array<std::array<int, 4>, 4>, 10>(curr_board);
     score_queue = new state_queue<int, 10>(score);
@@ -128,66 +128,58 @@ int board::get_next_max_tile() const { return next_max; }
 
 int board::get_curr_sum() const { return curr_sum; }
 
-bool board::next_left_possible() const {
-    return (curr_board != next_left);
-}
+bool board::next_left_possible() const { return (curr_board != next_left); }
 
-bool board::next_up_possible() const {
-    return (curr_board != next_up);
-}
+bool board::next_up_possible() const { return (curr_board != next_up); }
 
-bool board::next_right_possible() const {
-    return (curr_board != next_right);
-}
+bool board::next_right_possible() const { return (curr_board != next_right); }
 
-bool board::next_down_possible() const {
-    return (curr_board != next_down);
-}
+bool board::next_down_possible() const { return (curr_board != next_down); }
 
 int board::collapse(std::array<int*, 4> arr) {
-    int score_increment=0;
-    
-    //shift
+    int score_increment = 0;
+
+    // shift
     for (int i = 3; i > 0; i--) {
-        if (*arr[i]==0 && *arr[i - 1]!=0) {
+        if (*arr[i] == 0 && *arr[i - 1] != 0) {
             std::swap(*arr[i], *arr[i - 1]);
         }
     }
-    
-    for (int i = 0; i <3; i++) {
-        if (*arr[i]!=0 && *arr[i + 1]==0) {
+
+    for (int i = 0; i < 3; i++) {
+        if (*arr[i] != 0 && *arr[i + 1] == 0) {
             std::swap(*arr[i], *arr[i + 1]);
         }
-    }   
-    
-    //merge
+    }
+
+    // merge
     for (int i = 3; i > 0; i--) {
-        if (*arr[i]!=0 && *arr[i] == *arr[i - 1]) {
+        if (*arr[i] != 0 && *arr[i] == *arr[i - 1]) {
             *arr[i] += *arr[i - 1];
             score_increment += *arr[i - 1];
             *arr[i - 1] = 0;
             i--;
         }
     }
-    
-    //shift
+
+    // shift
     for (int i = 3; i > 0; i--) {
-        if (*arr[i]==0 && *arr[i - 1]!=0) {
+        if (*arr[i] == 0 && *arr[i - 1] != 0) {
             std::swap(*arr[i], *arr[i - 1]);
         }
     }
-    
-    for (int i = 0; i <3; i++) {
-        if (*arr[i]!=0 && *arr[i + 1]==0) {
+
+    for (int i = 0; i < 3; i++) {
+        if (*arr[i] != 0 && *arr[i + 1] == 0) {
             std::swap(*arr[i], *arr[i + 1]);
         }
-    }  
+    }
 
     return score_increment;
 }
 
 void board::look_left() {
-  next_left = curr_board;
+    next_left = curr_board;
     collapse({&next_left[0][3], &next_left[0][2], &next_left[0][1],
               &next_left[0][0]});
     collapse({&next_left[1][3], &next_left[1][2], &next_left[1][1],
@@ -199,19 +191,15 @@ void board::look_left() {
 }
 
 void board::look_up() {
-	next_up = curr_board;
-    collapse({&next_up[3][0], &next_up[2][0], &next_up[1][0],
-              &next_up[0][0]});
-    collapse({&next_up[3][1], &next_up[2][1], &next_up[1][1],
-              &next_up[0][1]});
-    collapse({&next_up[3][2], &next_up[2][2], &next_up[1][2],
-              &next_up[0][2]});
-    collapse({&next_up[3][3], &next_up[2][3], &next_up[1][3],
-              &next_up[0][3]}); 
+    next_up = curr_board;
+    collapse({&next_up[3][0], &next_up[2][0], &next_up[1][0], &next_up[0][0]});
+    collapse({&next_up[3][1], &next_up[2][1], &next_up[1][1], &next_up[0][1]});
+    collapse({&next_up[3][2], &next_up[2][2], &next_up[1][2], &next_up[0][2]});
+    collapse({&next_up[3][3], &next_up[2][3], &next_up[1][3], &next_up[0][3]});
 }
 
 void board::look_right() {
-	next_right = curr_board;
+    next_right = curr_board;
     collapse({&next_right[0][0], &next_right[0][1], &next_right[0][2],
               &next_right[0][3]});
     collapse({&next_right[1][0], &next_right[1][1], &next_right[1][2],
@@ -223,7 +211,7 @@ void board::look_right() {
 }
 
 void board::look_down() {
-	next_down = curr_board;
+    next_down = curr_board;
     collapse({&next_down[0][0], &next_down[1][0], &next_down[2][0],
               &next_down[3][0]});
     collapse({&next_down[0][1], &next_down[1][1], &next_down[2][1],
@@ -237,31 +225,27 @@ void board::look_down() {
 bool board::add_random_tile() {
     int tile_val = rand() % 10 == 0 ? 4 : 2;
     int blank_cnt = 0;
-    
+
     vector<pair<int, int>> v;
-    
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            if(curr_board[i][j]==0){
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (curr_board[i][j] == 0) {
                 v.push_back(make_pair(i, j));
                 blank_cnt++;
             }
         }
-        
     }
-    
 
     if (blank_cnt == 0) {
         return false;
     }
 
-    int tile_index = rand()%blank_cnt;
+    int tile_index = rand() % blank_cnt;
     int i = v[tile_index].first;
     int j = v[tile_index].second;
     curr_board[i][j] = tile_val;
-	return true;
+    return true;
 }
 
 int board::grid_max(const std::array<std::array<int, 4>, 4>& arr) {
--- src/main.cpp
+++ src/main.cpp
@@ -11,73 +11,72 @@ void print_grid(std::array<std::array<int, 4>, 4> curr_board) {
     for (int i = 0; i < 4; i++) {
         cout << "|";
         for (int j = 0; j < 4; j++) {
-            if(curr_board[i][j]) cout << curr_board[i][j] << "|";
-            else cout << " |";
+            if (curr_board[i][j])
+                cout << curr_board[i][j] << "|";
+            else
+                cout << " |";
         }
         cout << "\n---------" << endl;
     }
 }
 
-bool Move(board *S, int i){
-    if(i==0) {
-        cout << "left:\n"; 
-        if(S->next_left_possible()){
+bool Move(board *S, int i) {
+    if (i == 0) {
+        cout << "left:\n";
+        if (S->next_left_possible()) {
             S->move_left();
             return true;
-        }
-        else return false;
-    }
-    else if(i==1) {
-        cout << "up:\n"; 
-        if(S->next_up_possible()){
+        } else
+            return false;
+    } else if (i == 1) {
+        cout << "up:\n";
+        if (S->next_up_possible()) {
             S->move_up();
             return true;
-        }
-        else return false;
-    }
-    else if(i==2) {
-        cout << "down:\n"; 
-        if(S->next_down_possible()){
+        } else
+            return false;
+    } else if (i == 2) {
+        cout << "down:\n";
+        if (S->next_down_possible()) {
             S->move_down();
             return true;
-        }
-        else return false;
-    }
-    else if(i==3) {
-        cout <<"right:\n"; 
-        if(S->next_right_possible()){
+        } else
+            return false;
+    } else if (i == 3) {
+        cout << "right:\n";
+        if (S->next_right_possible()) {
             S->move_right();
             return true;
-        }
-        else return false;
+        } else
+            return false;
     }
-    
+
     return false;
 }
 
-void popl_assignment(){
+void popl_assignment() {
     board *S = new board();
     print_grid(S->get_curr_board());
     cout << "***************\n";
     cout << "score : " << S->get_curr_sum() << endl;
     cout << "***************\n";
 
-    while(S->get_curr_sum()<8){
-        
-        int i = rand()%4;
-        
-        while(!Move(S, i)){
-            i = rand()%4;
+    while (S->get_curr_sum() < 8) {
+        int i = rand() % 4;
+
+        while (!Move(S, i)) {
+            i = rand() % 4;
         }
-        
+
         print_grid(S->get_curr_board());
         cout << "***************\n";
         cout << "score : " << S->get_curr_sum() << endl;
         cout << "***************\n";
-        if(S->get_curr_sum()==8) break;
-        
-        else if(S->get_curr_sum()>8){
-            while(S->get_curr_sum()>8){
+        if (S->get_curr_sum() == 8)
+            break;
+
+        else if (S->get_curr_sum() > 8) {
+            while (S->get_curr_sum() > 8) {
                 S->undo();
                 print_grid(S->get_curr_board());
                 cout << "***************\n";
@@ -89,45 +88,45 @@ void popl_assignment(){
     delete S;
 }
 
-void play_2048(){
+void play_2048() {
     board S;
-    while(1){
+    while (1) {
         print_grid(S.get_curr_board());
         cout << "************\n";
         char c;
         cin >> c;
         cout << "************\n";
-        if(c=='a') {
+        if (c == 'a') {
             S.move_left();
         }
-        if(c=='s') {
+        if (c == 's') {
             S.move_down();
         }
-        if(c=='d') {
+        if (c == 'd') {
             S.move_right();
         }
-        if(c=='w') {
+        if (c == 'w') {
             S.move_up();
         }
-        if(c=='u') {
+        if (c == 'u') {
             S.undo();
         }
-        if(c=='r') {
+        if (c == 'r') {
             S.redo();
-        } 
-        if(c=='q') break;
+        }
+        if (c == 'q') break;
     }
 }
 
 int main() {
-    
     srand(0);
-    
+
     char c;
     cout << "Enter 'a' to solve assignment and 'p' to play game: ";
     cin >> c;
     cout << "You have chosen " << c << endl;
-    if(c=='a') popl_assignment();
-    else play_2048();
-    
+    if (c == 'a')
+        popl_assignment();
+    else
+        play_2048();
 }
